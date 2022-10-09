757cdb6143128914a2fb410cb1442e524c41a57a
edwinvillota <edwinvillota@hotmail.com>
2022-10-08 20:28:48 -0500

    Added pre-commit hook

--- web-client/src/components/templates/list/list.styles.ts
+++ web-client/src/components/templates/list/list.styles.ts
@@ -1,12 +1,12 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const List = styled.ul`
-    display: grid;
-    width: 100%;
-    row-gap: ${({theme}) => theme.sizes.m };
-`
+  display: grid;
+  width: 100%;
+  row-gap: ${({ theme }) => theme.sizes.m};
+`;
 
 export const ListItem = styled.li`
-    display: flex;
-    width: 100%;
-`
+  display: flex;
+  width: 100%;
+`;
